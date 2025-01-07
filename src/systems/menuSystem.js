42a3a8b045f004c57236a7896c0340c0f4291e5d
Colin Kiama <colinkiama@gmail.com>
2025-01-07 21:18:45 +0000

    Start creating MenuSystem class

--- src/systems/menuSystem.js
+++ src/systems/menuSystem.js
@@ -1,3 +1,4 @@
+import { Scene } from 'phaser';
 /**
  * @typedef {Object} MenuItem
  * @property {boolean|undefined} isInteractive - Determines if an item is selectable or not
@@ -15,9 +16,37 @@
 /**
  * @typedef {Object} Menu
  * @property {string} parent - `key` of the previous menu that led to this one
+ * @property {string} key - Unique identifier
  * @property {MenuTitle} title - The content that appears on the top of the menu
  * @property {string} summary
  * @property {Array<MenuItem>} items
  * @property {(Array<MenuItem>|undefined)} footerItems
  */
 
+export class MenuSystem {
+  scene;
+  menuMap;
+  firstMenuKey;
+
+  /**
+   * @param {Scene} scene
+   * @param {Array<Menu>} menus
+   * @param {string} firstMenuKey
+   */
+  constructor(scene, menus, firstMenuKey) {
+    this.scene = scene;
+    this.firstMenuKey = firstMenuKey;
+    this.menuMap = new Map();
+
+    for (const menu of menus) {
+      this.menuMap.set(menu.key, menu);
+    }
+  }
+
+  /**
+   * @param {string} key - Key of menu to switch to
+   */
+  push(key) {}
+
+  pop() {}
+}
