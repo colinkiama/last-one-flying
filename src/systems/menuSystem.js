f5244c31339ba8af105ac45530f19672a1fb74c1
Colin Kiama <colinkiama@gmail.com>
2025-01-08 19:35:20 +0000

    Set optional MenuSystem type properties as "optional"

--- src/systems/menuSystem.js
+++ src/systems/menuSystem.js
@@ -1,32 +1,33 @@
 import { Scene } from 'phaser';
 /**
  * @typedef {Object} MenuItem
- * @property {boolean|undefined} isInteractive - Determines if an item is selectable or not
+ * @property {boolean} [isInteractive] Determines if an item is selectable or not
  * @property {string} label
- * @property {function(): void} action - Logic to run when item is selected
+ * @property {function(): void} action Logic to run when item is selected
  */
 
 /**
  * @typedef {Object} MenuTitle
  * @property {"text"|"image"} type
- * @property {string} value - When `type` = "text"
+ * @property {string} value When `type` = "text"
  * it's the value of the text. When `type` = "image", it's the key of the image.
  */
 
 /**
  * @typedef {Object} Menu
- * @property {string} parent - `key` of the previous menu that led to this one
- * @property {string} key - Unique identifier
- * @property {MenuTitle} title - The content that appears on the top of the menu
- * @property {string} summary
+ * @property {string} [parent] `key` of the previous menu that led to this one
+ * @property {string} key Unique identifier
+ * @property {MenuTitle} title The content that appears on the top of the menu
+ * @property {string} [summary]
  * @property {Array<MenuItem>} items
- * @property {(Array<MenuItem>|undefined)} footerItems
+ * @property {(Array<MenuItem>)} [footerItems]
  */
 
 export class MenuSystem {
   scene;
   menuMap;
   firstMenuKey;
+  _currentMenuContainer;
 
   /**
    * @param {Scene} scene
@@ -41,6 +42,8 @@ export class MenuSystem {
     for (const menu of menus) {
       this.menuMap.set(menu.key, menu);
     }
+
+    console.log('MenuSystem:', this);
   }
 
   /**
@@ -49,4 +52,8 @@ export class MenuSystem {
   push(key) {}
 
   pop() {}
+
+  _renderMenu(menu, options) {
+    // TODO: Create menu to show on screen
+  }
 }
