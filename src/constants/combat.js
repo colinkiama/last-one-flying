0ce52b54cea3b2ea9864645b696b7ac8a533d037
Colin Kiama <colinkiama@gmail.com>
2025-02-07 00:08:56 +0000

    Slow down enemy fire rate. 2500ms delay per shot

--- src/constants/combat.js
+++ src/constants/combat.js
@@ -1,3 +1,4 @@
 export const LASER_SHOT_DELAY = 250; // In milliseconds
 export const ENEMY_HEIGHT = 24;
-export const ENEMY_MOVEMENT_SPEED = 75
+export const ENEMY_MOVEMENT_SPEED = 75;
+export const ENEMY_SHOT_DELAY = 2500;
