77a67878f1e710edddf72cf3502049f250a1e150
Colin Kiama <colinkiama@gmail.com>
2025-02-25 01:07:59 +0000

    Add LoopConfig type in preparation for looped music playback

--- src/systems/audioSystem.js
+++ src/systems/audioSystem.js
@@ -1,20 +1,47 @@
+import * as Phaser from 'phaser';
+/**
+ * @typedef {Object} LoopConfig
+ * @property {Phaser.Types.Sound.SoundConfig} initialPlayback
+ * @property {Phaser.Types.Sound.SoundConfig} loopPlayback
+ */
+
 export class AudioSystem {
+  /**
+   * @type {Phaser.Sound.BaseSoundManager}
+   */
   _soundManager;
+
+  /**
+   * @type {Map<string, LoopConfig>}
+   */
+  _loopConfigs;
 
   constructor(soundManager) {
     this._soundManager = soundManager;
+    this._loopConfigs = new Map();
   }
 
   play(key, options) {
     const audio = this.getOrAddAudio(key, options);
-    if (!audio) {
-      console.error(`Audio not found using the key: '${key}'`);
-    }
-
     audio.play();
   }
 
+  pause(key) {
+    const audio = this.get(key);
+    audio.play();
+  }
+
+  resume(key) {
+    const audio = this.get(key);
+    audio.resume();
+  }
+
   get(key) {
+    const audio = this.get(key);
+    if (!audio) {
+      throw new Error(`Audio not found using the key: '${key}'`);
+    }
+
     return this._soundManager.get(key);
   }
 
@@ -23,11 +50,7 @@ export class AudioSystem {
   }
 
   stop(key) {
-    const audio = this._soundManager.get(key);
-    if (!audio) {
-      console.error(`Audio not found using the key: '${key}'`);
-    }
-
+    const audio = this.get(key);
     audio.stop();
   }
 }
