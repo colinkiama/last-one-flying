1f0fa0ce9818746b10e01a6eb822ba218824c5e7
Colin Kiama <colinkiama@gmail.com>
2025-02-27 22:28:21 +0000

    Ensure that the Battle Theme loop occurs consistently

--- src/constants/audio.js
+++ src/constants/audio.js
@@ -7,14 +7,18 @@ export const AudioMarkerKey = {
   BATTLE_THEME_LOOP: 'battle-theme-loop',
 };
 
+const BATTLE_THEME_LENGTH = 109.761;
+const BATTLE_LOOP_MARKER_START = 6.3;
+const BATTLE_THEME_LOOP_DURATION = BATTLE_THEME_LENGTH - BATTLE_LOOP_MARKER_START;
+
 export const LOOP_MARKER_CONFIGS = {
   [AudioKey.BATTLE_THEME]: {
     name: AudioMarkerKey.BATTLE_THEME_LOOP,
-    start: 6.3, // In Seconds
-    duration: 101.7,
+    start: BATTLE_LOOP_MARKER_START, // In Seconds
+    duration: BATTLE_THEME_LOOP_DURATION,
     config: {
       loop: true,
-      seek: 6.3,
+      seek: BATTLE_LOOP_MARKER_START,
     }
   }
 };
